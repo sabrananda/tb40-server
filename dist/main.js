7cd8ab79b03aa967d1cae0b38109c2f806213f20
sabrananda <sabranandasatria@gmail.com>
2024-04-28 22:06:20 +0700

    build3

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({
-        origin: ['http://127.0.0.1:3000', 'https://tb40.vercel.app'],
-    });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     await app.listen(3001);
 }
 bootstrap();
