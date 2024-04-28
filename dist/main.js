f54cfea822242848d25e901118a2b4d5c47d7c22
sabrananda <sabranandasatria@gmail.com>
2024-04-29 00:54:36 +0700

    perbaikan cors2

--- dist/main.js
+++ dist/main.js
@@ -5,16 +5,6 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     try {
         const app = await core_1.NestFactory.create(app_module_1.AppModule);
-        app.enableCors({
-            origin: [
-                'https://tb40.vercel.app',
-                'http://192.168.43.13:3000',
-                'http://127.0.0.1:3000',
-                /\.vercel\.app$/,
-            ],
-            methods: ['GET', 'POST'],
-            credentials: true,
-        });
         await app.listen(3001);
     }
     catch (error) {
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-      //   allowedHeaders: ['Content-Type', 'Authorization'],
-    });
+    // app.enableCors({
+    //   origin: '*',
+    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    //   credentials: true,
+    //   //   allowedHeaders: ['Content-Type', 'Authorization'],
+    // });
     await app.listen(3001);
   } catch (error) {
     throw new Error(error);
