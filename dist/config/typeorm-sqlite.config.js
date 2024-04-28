cee1a8a2ed325414fb7a363c2d851de77897a9bf
sabrananda <sabranandasatria@gmail.com>
2024-04-28 23:20:55 +0700

    supabase

--- dist/config/typeorm-sqlite.config.js
+++ dist/config/typeorm-sqlite.config.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeormConfig = void 0;
 exports.typeormConfig = {
-    type: 'sqlite',
-    database: 'data/tb40DB',
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    type: 'postgres',
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+    port: 5432,
+    username: 'postgres.xnyvmyodwycaxalznecn',
+    password: 'tafsirbakat40',
+    database: 'postgres',
+    entities: [__dirname + '/../**/*.entity.{ts,js}'],
     synchronize: true,
-    migrations: ['dist/src/config/migrations/*{.ts,.js}'],
 };
 //# sourceMappingURL=typeorm-sqlite.config.js.map
--- src/config/typeorm-sqlite.config.ts
+++ src/config/typeorm-sqlite.config.ts
@@ -1,15 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-// export const typeormConfig: TypeOrmModuleOptions = {
-//   type: 'postgres',
-//   host: 'aws-0-ap-southeast-1.pooler.supabase.com',
-//   port: 5432,
-//   username: 'postgres.xnyvmyodwycaxalznecn',
-//   password: 'tafsirbakat40',
-//   database: 'postgres',
-//   entities: [__dirname + '/../**/*.entity.{ts,js}'],
-//   synchronize: true,
-// };
+export const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+  port: 5432,
+  username: 'postgres.xnyvmyodwycaxalznecn',
+  password: 'tafsirbakat40',
+  database: 'postgres',
+  entities: [__dirname + '/../**/*.entity.{ts,js}'],
+  synchronize: true,
+};
 // export const typeormConfig: TypeOrmModuleOptions = {
 //   type: 'postgres',
 //   host: 'localhost',
@@ -21,10 +21,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 //   synchronize: true,
 // };
 
-export const typeormConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'data/tb40DB',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
-  migrations: ['dist/src/config/migrations/*{.ts,.js}'],
-};
+// export const typeormConfig: TypeOrmModuleOptions = {
+//   type: 'sqlite',
+//   database: 'data/tb40DB',
+//   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//   synchronize: true,
+//   migrations: ['dist/src/config/migrations/*{.ts,.js}'],
+// };
