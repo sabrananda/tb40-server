48a2df95605231dfcdb17fc8c871cac8bbb48f97
sabrananda <sabranandasatria@gmail.com>
2024-04-28 22:51:00 +0700

    json function2

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule, { cors: true });
-    //   app.enableCors({
-    //     origin: ['http://127.0.0.1:3000', 'https://tb40.vercel.app'],
-    //     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    //     // credentials: true,
-    //   });
+    //   app.enableCors
     await app.listen(3001);
   } catch (error) {
     throw new Error(error);
