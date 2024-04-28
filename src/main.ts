2bd8e57cac8fea5338ac975a2037b8da9c109a8b
sabrananda <sabranandasatria@gmail.com>
2024-04-28 19:10:01 +0700

    open cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://tb40.vercel.app'],
-  });
+  app.enableCors();
 
   await app.listen(3001);
 }
