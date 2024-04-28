2ced45cd34d5d3dfe866db724443c711791d75c2
sabrananda <sabranandasatria@gmail.com>
2024-04-28 21:57:49 +0700

    cors4

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  //   app.enableCors({
-  //     origin: true,
-  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //     credentials: true,
-  //   });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://127.0.0.1:3000', 'https://tb40.vercel.app'],
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    // credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
