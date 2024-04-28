467a266bd12cda96a1813c750702a859b997feee
sabrananda <sabranandasatria@gmail.com>
2024-04-28 23:26:28 +0700

    origin cors2

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,14 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     try {
-        const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        app.enableCors({
+            origin: [
+                'https://tb40.vercel.app',
+                'http://192.168.43.13:3000',
+                'http://127.0.0.1:3000',
+            ],
+        });
         await app.listen(3001);
     }
     catch (error) {
