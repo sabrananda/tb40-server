d44723758989275add13ecee93fbe38eacdd7c0e
sabrananda <sabranandasatria@gmail.com>
2024-04-29 11:24:14 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,27 @@ import cors from 'cors';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.use(
-      cors({
-        origin: [
-          'https://tb40.vercel.app/',
-          'http://localhost:3000',
-          'https://tb40-sabranandas-projects.vercel.app',
-        ],
-        methods: ['GET', 'POST'],
-        // credentials: true,
-      }),
-    );
-    // app.enableCors({
-    //   origin: [
-    //     'https://tb40.vercel.app',
-    //     'http://localhost:3000',
-    //     'https://tb40-sabranandas-projects.vercel.app',
-    //   ],
-    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    //   credentials: true,
-    //   //   allowedHeaders: ['Content-Type', 'Authorization'],
-    // });
+    // app.use(
+    //   cors({
+    //     origin: [
+    //       'https://tb40.vercel.app/',
+    //       'http://localhost:3000',
+    //       'https://tb40-sabranandas-projects.vercel.app',
+    //     ],
+    //     methods: ['GET', 'POST'],
+    //     credentials: true,
+    //   }),
+    // );
+    app.enableCors({
+      origin: [
+        'https://tb40.vercel.app',
+        'http://localhost:3000',
+        'https://tb40-sabranandas-projects.vercel.app',
+      ],
+      methods: ['GET', 'POST'],
+      credentials: true,
+      //   allowedHeaders: ['Content-Type', 'Authorization'],
+    });
     await app.listen(3001);
   } catch (error) {
     throw new Error(error);
