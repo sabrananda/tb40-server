8e40335180e14fd1533dd6edda17f9683a47219e
sabrananda <sabranandasatria@gmail.com>
2024-04-29 11:01:13 +0700

    cors

--- src/v1/bakat40/bakat40.controller.ts
+++ src/v1/bakat40/bakat40.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { Bakat40DTO, Pernyataan } from './bakat40.dto';
 import { Bakat40Service } from './bakat40.service';
 
@@ -11,7 +11,18 @@ export class Bakat40Controller {
     return this.bakat40Service.getBakat40();
   }
   @Get('/pernyataan')
-  getPernyataan(): Promise<Pernyataan[]> {
-    return this.bakat40Service.getPernyataan();
+  getPernyataan(@Res() res: Response): Promise<Pernyataan[]> {
+    const pernyataan = this.bakat40Service.getPernyataan();
+    res.headers.set('Access-Control-Allow-Credentials', 'true');
+    res.headers.set('Access-Control-Allow-Origin', '*'); // replace this your actual origin
+    res.headers.set(
+      'Access-Control-Allow-Methods',
+      'GET,DELETE,PATCH,POST,PUT',
+    );
+    res.headers.set(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    return pernyataan;
   }
 }
