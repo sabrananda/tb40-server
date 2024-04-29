05a600a2d584c4d09e2a20cc8fb0d249c27761f8
sabrananda <sabranandasatria@gmail.com>
2024-04-29 11:30:27 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
+// import cors from 'cors';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
@@ -15,16 +15,16 @@ async function bootstrap() {
     //     credentials: true,
     //   }),
     // );
-    app.enableCors({
-      origin: [
-        'https://tb40.vercel.app',
-        'http://localhost:3000',
-        'https://tb40-sabranandas-projects.vercel.app',
-      ],
-      methods: ['GET', 'POST'],
-      credentials: true,
-      //   allowedHeaders: ['Content-Type', 'Authorization'],
-    });
+    // app.enableCors({
+    //   origin: [
+    //     'https://tb40.vercel.app',
+    //     'http://localhost:3000',
+    //     'https://tb40-sabranandas-projects.vercel.app',
+    //   ],
+    //   methods: ['GET', 'POST'],
+    //   credentials: true,
+    //   //   allowedHeaders: ['Content-Type', 'Authorization'],
+    // });
     await app.listen(3001);
   } catch (error) {
     throw new Error(error);
