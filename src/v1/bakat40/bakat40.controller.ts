342dae6d0020284d517577d855b28546714aa87d
sabrananda <sabranandasatria@gmail.com>
2024-04-29 11:11:51 +0700

    cors

--- src/v1/bakat40/bakat40.controller.ts
+++ src/v1/bakat40/bakat40.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { Bakat40DTO, Pernyataan } from './bakat40.dto';
 import { Bakat40Service } from './bakat40.service';
-import { Response } from 'express';
+
 @Controller('v1/bakat40')
 export class Bakat40Controller {
   constructor(private readonly bakat40Service: Bakat40Service) {}
@@ -11,20 +11,7 @@ export class Bakat40Controller {
     return this.bakat40Service.getBakat40();
   }
   @Get('/pernyataan')
-  async getPernyataan(@Res() res: Response): Promise<Response> {
-    const pernyataan = await this.bakat40Service.getPernyataan();
-    res.appendHeader('Access-Control-Allow-Credentials', 'true');
-    res.appendHeader('Access-Control-Allow-Origin', 'https://tb40.vercel.app'); // replace this your actual origin
-    res.appendHeader(
-      'Access-Control-Allow-Methods',
-      'GET,DELETE,PATCH,POST,PUT',
-    );
-    res.appendHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    );
-    const body = JSON.stringify(pernyataan);
-    res.json(body);
-    return res;
+  getPernyataan(): Promise<Pernyataan[]> {
+    return this.bakat40Service.getPernyataan();
   }
 }
