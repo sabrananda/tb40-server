6d03226784db0362ccea43382a66bb3900681e39
sabrananda <sabranandasatria@gmail.com>
2024-04-30 14:20:55 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule, { cors: true });
-
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    });
     // app.enableCors({
     //   //   origin: true,
     //   origin: [
