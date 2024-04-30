866f3ee3dca49a65e2f36a5211e4e14acf589767
sabrananda <sabranandasatria@gmail.com>
2024-04-30 14:34:50 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,39 @@ import { NextFunction, Request, Response } from 'express';
 import { AppModule } from './app.module';
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule, { cors: true });
-    app.use((req: Request, res: Response, next: NextFunction) => {
-      res.setHeader('Access-Control-Allow-Origin', 'https://tb40.vercel.app');
-      res.setHeader('Access-Control-Allow-Methods', [
-        'POST',
-        'GET',
-        'PUT',
-        'DELETE',
-      ]);
-      res.setHeader('Access-Control-Allow-Headers', [
-        'Origin',
-        'Content-Type',
-        'X-Auth-Token',
-      ]);
-      next();
-    });
-    // app.enableCors({
-    //   origin: [
-    //     'https://tb40.vercel.app',
-    //     'http://192.168.110.198:3000',
-    //     'http://localhost:3000',
-    //     'https://tb40-8o264tsp5-sabranandas-projects.vercel.app',
-    //   ],
-    //   allowedHeaders: [
-    //     'Content-Type',
-    //     'Authorization',
-    //     'X-Requested-With',
-    //     'Accept',
+    const app = await NestFactory.create(AppModule);
+    // app.use((req: Request, res: Response, next: NextFunction) => {
+    //   res.setHeader('Access-Control-Allow-Origin', 'https://tb40.vercel.app');
+    //   res.setHeader('Access-Control-Allow-Methods', [
+    //     'POST',
+    //     'GET',
+    //     'PUT',
+    //     'DELETE',
+    //   ]);
+    //   res.setHeader('Access-Control-Allow-Headers', [
     //     'Origin',
-    //   ],
-    //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    //   credentials: true,
+    //     'Content-Type',
+    //     'X-Auth-Token',
+    //   ]);
+    //   next();
     // });
+    app.enableCors({
+      origin: [
+        'https://tb40.vercel.app',
+        'http://192.168.110.198:3000',
+        'http://localhost:3000',
+        'https://tb40-8o264tsp5-sabranandas-projects.vercel.app',
+      ],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    });
 
     await app.listen(3002);
   } catch (error) {
