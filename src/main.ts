1b6e17fd94c943557941b3bff67640629bfb2304
sabrananda <sabranandasatria@gmail.com>
2024-04-30 11:42:07 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    // app.enableCors({
-    //   origin: [
-    //     'https://tb40.vercel.app',
-    //     'http://localhost:3000',
-    //     'https://tb40-sabranandas-projects.vercel.app',
-    //   ],
-    //   methods: ['GET', 'POST'],
-    //   credentials: true,
-    //   //   allowedHeaders: ['Content-Type', 'Authorization'],
-    // });
+
     app.enableCors({
       //   origin: true,
       origin: [
@@ -20,8 +11,14 @@ async function bootstrap() {
         'http://192.168.110.198:3000',
         'https://tb40-8o264tsp5-sabranandas-projects.vercel.app',
       ],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      methods: ['GET', 'POST'],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     });
     await app.listen(3002);
