6c1df10e8a97f35475336054daf31728d33fb681
sabrananda <sabranandasatria@gmail.com>
2024-04-30 15:26:21 +0700

    cors

--- src/v1/result/result.controller.ts
+++ src/v1/result/result.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Req, Res } from '@nestjs/common';
 import { PostAssesmentDTO } from './result.dto';
 import { ResultService } from './result.service';
-import { Response } from 'express';
+// import { Response } from 'express';
 
 @Controller('v1/result')
 export class ResultController {
@@ -15,21 +15,24 @@ export class ResultController {
     return this.resultService.getDefaultResultStates();
   }
   @Post()
+  @Header('Access-Control-Allow-Origin', '*')
   async getAssessmentResult(
     @Body() body: PostAssesmentDTO,
-    @Req() req: Request,
-    @Res() res: Response,
-  ): Promise<Response<any, Record<string, any>>> {
+    // @Req() req: Request,
+    // @Res() res: Response,
+  ): Promise<string> {
+    // res.headers
+    //   ): Promise<Response<any, Record<string, any>>> {
     // app.use((req: Request, res: Response) => {
     //     res.headers.set('Access-Control-Allow-Origin', 'https://tb40.vercel.app');
     //     res.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     //     res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
     //   });
-    const out = await this.resultService.getAssessmentResult(body);
-    res
-      .setHeader('Access-Control-Allow-Origin', '*')
-      .setHeader('Access-Control-Allow-Method', 'POST,GET,PUT,OPTIONS,DELETE')
-      .json(out);
-    return res;
+    // const out = await this.resultService.getAssessmentResult(body);
+    // res
+    //   .setHeader('Access-Control-Allow-Origin', '*')
+    //   .setHeader('Access-Control-Allow-Method', 'POST,GET,PUT,OPTIONS,DELETE')
+    //   .json(out);
+    return await this.resultService.getAssessmentResult(body);
   }
 }
